533aa1a06705c962b2309bbd4239a0ccfc08b299
lintcoder <troyhit@126.com>
2018-04-25 10:16:22 +0800

    Replace fmt output with log, use goroutine for downloading any category of albums.

--- main.go
+++ main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"github.com/PuerkitoBio/goquery"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
 	"os"
+	"runtime"
 	"strconv"
 	"strings"
 	"sync"
@@ -23,9 +23,11 @@ const (
 var wg sync.WaitGroup
 
 func main() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+
 	doc, err := goquery.NewDocument(BASE_URL)
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
 		return
 	}
 
@@ -44,56 +46,49 @@ func main() {
 	})
 
 	if os.MkdirAll(BASE_DIR, DIR_MODE) != nil {
-		fmt.Println("mkdir gallery failed")
+		log.Println("mkdir gallery failed")
 		return
 	}
 
-	for i := 0; i < 1; i++ {
-		dir := BASE_DIR + "/" + title[i]
-		os.MkdirAll(dir, DIR_MODE)
-		doc, err = goquery.NewDocument(links[i])
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-
-		parseImgUrl(doc, dir)
-
-		link, ok := doc.Find("div.pagination").Find("a").Eq(0).Attr("href")
-		for ok {
-			fmt.Println("next page: ", link)
-			doc, err = goquery.NewDocument(link)
-			if err != nil {
-				fmt.Println(err)
-				break
-			}
-			parseImgUrl(doc, dir)
-
-			link, ok = doc.Find("div.pagination").Find("a").Eq(1).Attr("href")
-			if !ok {
-				break
-			}
-		}
+	for i := 0; i < len(title); i++ {
+		wg.Add(1)
+		go downloadOneCategory(title[i], links[i])
 	}
 
 	wg.Wait()
 }
 
+func downloadOneCategory(title, links string) {
+	defer wg.Done()
+
+	dir := BASE_DIR + "/" + title
+	os.MkdirAll(dir, DIR_MODE)
+	doc, err := goquery.NewDocument(links)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	parseImgUrl(doc, dir)
+
+	link, ok := doc.Find("div.pagination").Find("a").Eq(0).Attr("href")
+	for ok {
+		log.Println("next page: ", link)
+		doc, err = goquery.NewDocument(link)
+		if err != nil {
+			log.Println(err)
+			break
+		}
+		parseImgUrl(doc, dir)
+
+		link, ok = doc.Find("div.pagination").Find("a").Eq(1).Attr("href")
+	}
+}
+
 func parseImgUrl(doc *goquery.Document, dir string) {
 	doc.Find("div.node").Each(func(i int, s *goquery.Selection) {
-		//		if imgUrl, ok := s.Find("img").Attr("src"); ok {
-		//			if !strings.Contains(imgUrl, "adobe") {
-		//				if pos := strings.LastIndex(imgUrl, "-"); pos != -1 {
-		//					pos2 := strings.LastIndex(imgUrl, ".")
-		//					oriImgUrl := imgUrl[:pos] + imgUrl[pos2:]
-		//					fmt.Println(oriImgUrl)
-		//					//wg.Add(1)
-		//					//go downloadImg(oriImgUrl, dir)
-		//				}
-		//			}
 		s.Find("div.actions").Find("a").Each(func(j int, ss *goquery.Selection) {
 			if imgUrl, ok := ss.Attr("download"); ok {
-				fmt.Println(imgUrl)
 				wg.Add(1)
 				go downloadImg(imgUrl, dir)
 			}
@@ -123,19 +118,19 @@ func downloadImg(imgUrl, dir string) {
 
 	respdata, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		log.Println("read img data failed: ", err)
+		log.Println("read img data failed: ", imgUrl, err)
 		return
 	}
 	image, err := os.Create(filename)
 	if err != nil {
-		log.Println("create img file failed: ", err)
+		log.Println("create img file failed: ", filename, err)
 		return
 	}
 
 	defer image.Close()
 	_, err = image.Write(respdata)
 	if err != nil {
-		log.Println("write img file failed: ", filename)
+		log.Println("write img file failed: ", filename, err)
 		os.Remove(filename)
 		return
 	}
